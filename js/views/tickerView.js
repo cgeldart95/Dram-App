dfe5f0984890730db9de18390d30ff782b32df0d
cgeldart95 <connor.geldart95@gmail.com>
2025-06-17 18:57:33 +0100

    Updated the filepaths

--- js/views/tickerView.js
+++ js/views/tickerView.js
@@ -1,14 +1,14 @@
-export const addHandlerTicker = function (handler) {
-  document.addEventListener(
-    'DOMContentLoaded',
-    function () {
-      const homeView = document.querySelector('#home');
-      if (
-        homeView &&
-        !homeView.classList.contains('hidden')
-      ) {
-        handler(); // This is startTicker
-      }
-    }
-  );
-};
+// export const addHandlerTicker = function (handler) {
+//   document.addEventListener(
+//     'DOMContentLoaded',
+//     function () {
+//       const homeView = document.querySelector('#home');
+//       if (
+//         homeView &&
+//         !homeView.classList.contains('hidden')
+//       ) {
+//         handler(); // This is startTicker
+//       }
+//     }
+//   );
+// };
